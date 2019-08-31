5b9f96335fe77227f5fe07062af19d9fe45de9ae
Alaa Kasso <akasso@live.co.uk>
2019-08-31 21:10:42 +0100

    updating site with menu changes

--- script/main.js
+++ script/main.js
@@ -7,6 +7,15 @@ function changeMenuBackround() {
     if (this.document.body.scrollTop > h || this.document.documentElement.scrollTop > h) {
         this.document.getElementById("menu").className = "menu-down";
     } else {
-        this.document.getElementById("menu").className ="";
+        this.document.getElementById("menu").className = "";
+    }
+}
+
+function menuToggle() {
+    var x = this.document.getElementById("menu-btns")
+    if (x.className === "menu-btns") {
+        x.className += " show";
+    } else {
+        x.className = "menu-btns";
     }
 }
